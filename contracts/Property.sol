99b4700d3d829262e627cb24c5e2523db291ca63
"Arundhati Gunupuru" <"arundhati.gunu@gmail.com">
2022-04-11 15:14:42 +0530

    Added the buy feature to the property

--- contracts/Property.sol
+++ contracts/Property.sol
@@ -2,12 +2,12 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-// A contract to change the ownership of your properties
+// A contract to add your property details,
+// to sell and change the ownership of your properties 
 contract PropertyProject {
 
     struct Property {
         address owner;
-        uint id;
         string name;
         uint value;
         uint area;
@@ -15,33 +15,38 @@ contract PropertyProject {
 
 // all the properties listed for one particular address in an array,
 // getter not required as tagged public
-    mapping(address => Property[]) public properties;
+    mapping(address => mapping(uint => Property)) public properties;
 
-// add your properties using this function
+// events once amount is paid
+    event paid(address, uint);
+
+// add your property details using this function
     function addProperties(
         uint _id,
         string calldata _name,
         uint _value,
         uint _area
     ) public {
-        properties[msg.sender].push(Property(
-            msg.sender, 
-            _id,
-            _name,
-            _value,
-            _area
-        )); 
+        properties[msg.sender][_id].owner = msg.sender;
+        properties[msg.sender][_id].name = _name;
+        properties[msg.sender][_id].value = _value;
+        properties[msg.sender][_id].area = _area;
     }
 
-// can change the ownership of one of your properties to another address
-    function changePropertyOwner(uint _index, address _addr) public {
-        // the properties of only the msg.sender will be checked.
-        uint length = properties[msg.sender].length;
-        properties[msg.sender][_index].owner = _addr;
-        properties[_addr].push(properties[msg.sender][_index]);
-        // delete properties[msg.sender][_index];
-        // can replace the last property with the property to be deleted and then pop()
-        properties[msg.sender][_index] = properties[msg.sender][length - 1];
-        properties[msg.sender].pop();
+// can change the ownership of one of your properties to another after the amount has been paid
+    function changePropertyOwner(uint _id, address _addrtotransfer) public {
+        Property memory propertyTransferred = properties[msg.sender][_id];
+        require(propertyTransferred.owner == msg.sender, "not owner!");
+        propertyTransferred.owner = _addrtotransfer;
+        properties[_addrtotransfer][_id] = propertyTransferred;
+        delete properties[msg.sender][_id];
+    }
+
+// pay the amount to the owner address by checking the value of the property
+    function BuyProperty(address payable _ownerAddr, uint _id) public payable  {
+        uint _amountToBePaid = properties[_ownerAddr][_id].value;
+        require(msg.value == _amountToBePaid, "please pay according to the value of the property");
+        _ownerAddr.transfer(msg.value);
+        emit paid(_ownerAddr, msg.value);
     }
 }
